e9856c2dccaad0e19ae1c133b78f02e59a63d119
yasirraheel <gamsgo@gmail.com>
2025-12-10 08:41:02 +0500

    Fix Babel preset for JSX render

--- index.php
+++ index.php
@@ -42,7 +42,7 @@
   </head>
   <body>
     <div id="root"></div>
-    <script type="text/babel">
+    <script type="text/babel" data-presets="react">
       const { useState, useEffect, useCallback } = React;
       const { createRoot } = ReactDOM;
 
